d7e122fa1dcf6dc28af3bcef2f1e43de622fad38
eyob j <jemijobs22@gmail.com>
2024-10-30 17:09:47 +0300

    first commt

---
+++ shaders/smoke/fragment.glsl
@@ -0,0 +1,37 @@
+
+varying vec2 vUv;
+
+uniform sampler2D uPerlinTexture;
+uniform float uTime;
+
+
+void main(){
+
+    vec2 smokeUv = vUv;
+    smokeUv.x *= .5;
+    smokeUv.y *= .3;
+    smokeUv.y += -(uTime * .03);
+
+    float smoke = texture(uPerlinTexture, smokeUv).r;
+
+    //smoke remap
+
+    smoke = smoothstep(.4, 1., smoke);
+
+    smoke *= smoothstep( 0., .1, vUv.x );
+    smoke *= smoothstep( 1., .9, vUv.x );
+    smoke *= smoothstep( 0., .1, vUv.y );
+    smoke *= smoothstep( 1., .4, vUv.y );
+
+    // smoke = 1.;
+    // smoke = smoothstep(0., 0.1, vUv.x);
+    // smoke *= smoothstep(1., 0.9, vUv.x);
+    // smoke *= smoothstep(1., 0.9, vUv.y);
+
+
+
+    gl_FragColor =  vec4( .6, .3, .4, smoke);
+
+    #include <colorspace_fragment>    
+    #include <tonemapping_fragment>
+}
